d677be0c8d782eb3beecd4d743d4ab5ec1861313
max <maxwickhammonro@gmail.com>
2021-09-03 15:18:58 +0100

    recursive questions

--- mongo_models/questionnaire_model.py
+++ mongo_models/questionnaire_model.py
@@ -1,6 +1,6 @@
 from mongoengine import Document
 from mongoengine.document import EmbeddedDocument
-from mongoengine.fields import EmbeddedDocumentListField, EnumField, StringField, ObjectIdField
+from mongoengine.fields import EmbeddedDocumentListField, EnumField, ListField, StringField, ObjectIdField
 from enum import Enum
 from bson.objectid import ObjectId
 
@@ -12,12 +12,24 @@ class Question_Type(Enum):
 
 class Question(EmbeddedDocument):
     oid = ObjectIdField(required=True, default = ObjectId, primary_key=True)
-    question_type = EnumField(Question_Type)
+    question_type = EnumField(Question_Type, required = True)
+    text = StringField(required = True)
     scale = StringField()
-    text = StringField()
+    options = ListField()
+    follow_up_questions = ListField(ListField('Question'))
 
 class Questionnaire(Document):
     oid = ObjectIdField(required=True, default = ObjectId, primary_key=True)
     title = StringField()
     questions = EmbeddedDocumentListField(Question)
 
+class SymptomTile(EmbeddedDocument):
+    oid = ObjectIdField(required=True, default = ObjectId, primary_key=True)
+    logo = StringField()
+    title = StringField(required = True)
+    follow_up_questions = EmbeddedDocumentListField(Question)
+
+class SymptomTileQuestionnaire(Document):
+    oid = ObjectIdField(required=True, default = ObjectId, primary_key=True)
+    title = StringField(required = True)
+    tiles = EmbeddedDocumentListField(SymptomTile)
